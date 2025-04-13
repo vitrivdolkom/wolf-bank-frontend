9e39ff0e0f8b998b711bf42cd3ca0747c3214f2d
vitrivdolkom <vdy0555@gmail.com>
2025-04-13 18:43:40 +0700

    add register link

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,18 +1,22 @@
 import { ROUTES } from '@/utils/constants';
 
 const Page = () => (
-  <div className='flex justify-center items-center h-screen'>
+  <div className='flex flex-col justify-center items-center min-h-screen bg-base-200 px-4 rounded-lg shadow-xl'>
+    <div>
+      <a href={ROUTES.USERS} className='btn btn-ghost'>
+        Я сотрудник
+      </a>
+
+      <a href={ROUTES.MAIN} className='btn btn-ghost'>
+        Я клиент
+      </a>
+    </div>
+
     <a
-      href={ROUTES.USERS}
-      className={`flex flex-col items-center justify-center w-20 h-full transition-all duration-200 ${'text-base-content/70 hover:text-primary hover:scale-105'}`}
+      href='http://localhost:8082/register?client_id=wem7LcxWDUArXEm-0e4nsEjkwsroaXU_&redirect_uri=http://localhost:3000/&response_type=code'
+      className='btn btn-neutral mt-5'
     >
-      <span className='text-xs mt-1'>Я сотрудник</span>
-    </a>
-    <a
-      href={ROUTES.MAIN}
-      className={`flex flex-col items-center justify-center w-20 h-full transition-all duration-200 ${'text-base-content/70 hover:text-primary hover:scale-105'}`}
-    >
-      <span className='text-xs mt-1'>Я клиент</span>
+      Создать аккаунт
     </a>
   </div>
 );
